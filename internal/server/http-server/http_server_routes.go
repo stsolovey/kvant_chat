204ef2fca20957fd4e36cddc8e91ab07b20fc660
stsolovey <st.solovey@gmail.com>
2024-05-19 12:16:55 +0300

    renamed

---
+++ internal/server/http-server/http_server_routes.go
@@ -0,0 +1,37 @@
+package httpserver
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/sirupsen/logrus"
+	"github.com/stsolovey/kvant_chat/internal/app/handler"
+	"github.com/stsolovey/kvant_chat/internal/app/service"
+	"github.com/stsolovey/kvant_chat/internal/middleware"
+	"golang.org/x/time/rate"
+)
+
+func configureRoutes(
+	r chi.Router,
+	log *logrus.Logger,
+	usersServ service.UsersServiceInterface,
+	authServ service.AuthServiceInterface,
+) {
+	authHandler := handler.NewAuthHandler(authServ, log)
+	usersHandler := handler.NewUsersHandler(usersServ, log)
+
+	const (
+		loginRequestsPerSecond = 1
+		loginBurstSize         = 5
+		regisRequestsPerSecond = 1
+		regisBurstSize         = 3
+	)
+
+	loginLimiter := rate.NewLimiter(loginRequestsPerSecond, loginBurstSize)
+	regisLimiter := rate.NewLimiter(regisRequestsPerSecond, regisBurstSize)
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Route("/user", func(r chi.Router) {
+			r.With(middleware.RateLimiterMiddleware(loginLimiter)).Post("/login", authHandler.Login)
+			r.With(middleware.RateLimiterMiddleware(regisLimiter)).Post("/register", usersHandler.RegisterUser)
+		})
+	})
+}
