f2063a4e945a48ddf7db03385a45d276179ba158
stsolovey <st.solovey@gmail.com>
2024-05-20 22:11:34 +0300

    authService: token&user messages and rooms processing added

--- internal/server/tcp-server/tcpserver.go
+++ internal/server/tcp-server/tcpserver.go
@@ -9,35 +9,36 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/sirupsen/logrus"
+	"github.com/stsolovey/kvant_chat/internal/app/service"
 	"github.com/stsolovey/kvant_chat/internal/config"
 	"github.com/stsolovey/kvant_chat/internal/models"
 )
 
 type Server struct {
-	cfg       *config.Config
-	log       *logrus.Logger
-	rooms     map[string]*models.Room
-	mutex     *sync.Mutex
-	listener  net.Listener
-	connUsers map[net.Conn]*models.User
+	cfg         *config.Config
+	log         *logrus.Logger
+	rooms       map[string]*models.Room
+	mutex       *sync.Mutex
+	listener    net.Listener
+	connUsers   map[net.Conn]*models.User
+	authService *service.AuthService
 }
 
-func CreateServer(config *config.Config, logger *logrus.Logger) *Server {
+func CreateServer(
+	config *config.Config,
+	logger *logrus.Logger,
+	authService *service.AuthService,
+) *Server {
 	return &Server{
-		cfg:       config,
-		log:       logger,
-		rooms:     make(map[string]*models.Room),
-		mutex:     &sync.Mutex{},
-		connUsers: make(map[net.Conn]*models.User),
+		cfg:         config,
+		log:         logger,
+		rooms:       make(map[string]*models.Room),
+		mutex:       &sync.Mutex{},
+		connUsers:   make(map[net.Conn]*models.User),
+		authService: authService,
 	}
-}
-
-func (s *Server) getUserFromConn(conn net.Conn) *models.User {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
-	return s.connUsers[conn]
 }
 
 func (s *Server) Start(ctx context.Context) error {
@@ -100,109 +101,113 @@ func (s *Server) handleConnection(conn net.Conn) {
 
 	clientReader := bufio.NewReader(conn)
 
+	token, err := clientReader.ReadString('\n')
+	if err != nil {
+		s.log.WithError(err).Error("Failed to read the authentication token")
+		conn.Write([]byte("Failed to read token\n"))
+
+		return
+	}
+
+	token = strings.TrimSpace(token)
+
+	jwtToken, err := s.authService.ValidateToken(token)
+	if err != nil {
+		s.log.WithError(err).Error("Authentication failed")
+		conn.Write([]byte("Authentication failed\n"))
+
+		return
+	}
+
+	username := jwtToken.Claims.(jwt.MapClaims)["username"].(string)
+
+	user, err := s.authService.GetUserByUsername(context.Background(), username)
+	if err != nil {
+		s.log.WithError(err).Error("Failed to retrieve user data")
+		conn.Write([]byte("Failed to retrieve user data\n"))
+
+		return
+	}
+
+	s.log.Infof("User %s authenticated successfully", user.UserName)
+	s.mutex.Lock()
+	s.connUsers[conn] = user
+	s.joinRoom("general", user, conn)
+	s.mutex.Unlock()
+	conn.Write([]byte("Welcome to the chat server!\n"))
+
 	for {
 		message, err := clientReader.ReadString('\n')
 		if err != nil {
 			s.log.WithError(err).Error("Error reading from client")
+			s.leaveRoom("general", user)
 
 			return
 		}
 
-		message = strings.TrimSpace(message)
-		if message == "" {
-			continue
-		}
+		s.processCommand(user, strings.TrimSpace(message), conn)
+	}
+}
 
-		switch {
-		case strings.HasPrefix(message, "/join "):
-			s.handleJoin(conn, message[len("/join "):])
-		case strings.HasPrefix(message, "/msg "):
-			s.handleMessage(conn, message[len("/msg "):])
-		default:
-			if _, err := conn.Write([]byte("Unknown command\n")); err != nil {
-				s.log.WithError(err).Error("Error writing to client")
+func (s *Server) processCommand(user *models.User, command string, conn net.Conn) {
+	switch {
+	case strings.HasPrefix(command, "/join "):
+		roomName := command[len("/join "):]
+		s.joinRoom(roomName, user, conn)
+	case strings.HasPrefix(command, "/leave "):
+		roomName := command[len("/leave "):]
+		s.leaveRoom(roomName, user)
+	case strings.HasPrefix(command, "/msg "):
+		messageContent := command[len("/msg "):]
+		s.handleMessage(user, messageContent, conn)
+	default:
+		conn.Write([]byte("Unknown command\n"))
+	}
+}
 
-				return
+func (s *Server) handleMessage(user *models.User, message string, conn net.Conn) {
+	s.mutex.Lock()
+
+	defer s.mutex.Unlock()
+
+	if conn == nil {
+		s.log.Warn("Connection is nil, which is unexpected")
+	}
+
+	room, exists := s.rooms["general"]
+	if !exists {
+		s.log.Error("General room does not exist")
+		return
+	}
+
+	for member, memberConn := range room.Members {
+		if member != user { // exclude sender
+			messageToSend := fmt.Sprintf("%s: %s\n", user.UserName, message)
+			if _, err := memberConn.Write([]byte(messageToSend)); err != nil {
+				s.log.WithFields(logrus.Fields{
+					"room": room.Name,
+					"user": user.UserName,
+				}).WithError(err).Error("Failed to send message to room member")
 			}
 		}
 	}
 }
 
-func (s *Server) handleJoin(conn net.Conn, roomName string) {
-	s.mutex.Lock()
-
-	defer s.mutex.Unlock()
-
-	user := s.getUserFromConn(conn)
-	if user == nil {
-		errorMessage := "Error: Unable to identify user\n"
-
-		s.log.Error("Unable to identify user from connection")
-
-		if _, err := conn.Write([]byte(errorMessage)); err != nil {
-			s.log.WithError(err).Error("Failed to send identification error to client")
-		}
-
-		return
-	}
-
-	if _, ok := s.rooms[roomName]; !ok {
+func (s *Server) joinRoom(roomName string, user *models.User, conn net.Conn) {
+	if _, exists := s.rooms[roomName]; !exists {
 		s.rooms[roomName] = &models.Room{
 			Name:    roomName,
 			Members: make(map[*models.User]net.Conn),
 		}
 	}
 
-	room := s.rooms[roomName]
-	room.Members[user] = conn
-
-	joinMessage := fmt.Sprintf("Joined room: %s\n", roomName)
-
-	s.log.Infof("User %s joined room: %s", user.UserName, roomName)
-
-	if _, err := conn.Write([]byte(joinMessage)); err != nil {
-		s.log.WithError(err).Error("Failed to send room join confirmation to client")
-	}
+	s.rooms[roomName].Members[user] = conn
+	s.log.Infof("User %s joined the room %s", user.UserName, roomName)
 }
 
-func (s *Server) handleMessage(conn net.Conn, message string) {
-	s.mutex.Lock()
-
-	defer s.mutex.Unlock()
-
-	user := s.getUserFromConn(conn)
-	if user == nil {
-		errMsg := "Error: User not identified\n"
-
-		s.log.Error("User not identified from connection")
-
-		if _, err := conn.Write([]byte(errMsg)); err != nil {
-			s.log.WithError(err).Error("Failed to write user identification error to client")
-		}
-
-		return
-	}
-
-	for _, room := range s.rooms {
-		if userConn, ok := room.Members[user]; ok { // checks if user is in the room
-			for _, memberConn := range room.Members {
-				if memberConn != userConn { // checkint that sender does not get own message
-					messageToSend := message + "\n"
-					if _, err := memberConn.Write([]byte(messageToSend)); err != nil {
-						s.log.WithFields(logrus.Fields{ // failing to send a message to a room member
-							"room": room.Name,
-							"user": user.UserName,
-						}).WithError(err).Error("Failed to send message to room member")
-					} else {
-						// successful message delivery
-						s.log.WithFields(logrus.Fields{
-							"room":    room.Name,
-							"user":    user.UserName,
-							"message": message,
-						}).Info("Message sent to room member")
-					}
-				}
-			}
-		}
+func (s *Server) leaveRoom(roomName string, user *models.User) {
+	if room, exists := s.rooms[roomName]; exists {
+		delete(room.Members, user)
+		s.log.Infof("User %s left the room %s", user.UserName, roomName)
 	}
 }
