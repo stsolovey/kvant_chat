8b295a9dad7185400ff57187506fb7e9caf2b527
stsolovey <st.solovey@gmail.com>
2024-05-20 22:06:08 +0300

    register and login with token returnning added

--- cmd/chat_client/main.go
+++ cmd/chat_client/main.go
@@ -1,69 +1,110 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
-	"context"
-	"errors"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/sirupsen/logrus"
-)
-
-var (
-	errUsernameEmpty = errors.New("username cannot be empty")
-	errStatusNotOK   = errors.New("wrong status")
+	"github.com/stsolovey/kvant_chat/internal/logger"
+	"github.com/stsolovey/kvant_chat/internal/models"
 )
 
 func main() {
-	log := logrus.New()
+	log := logger.NewTextFormat()
 
-	os.Stdout.WriteString("Please enter your username: ")
+	reader := bufio.NewReader(os.Stdin)
+	serverURL := "http://localhost:8080/api/v1/user"
 
-	var username string
+	log.Info("Choose an option:")
+	log.Info("1: Register")
+	log.Info("2: Login")
 
-	fmt.Scanln(&username)
+	fmt.Print("Option: ")
+	option, _ := reader.ReadString('\n')
 
-	if username == "" {
-		log.WithError(errUsernameEmpty).Panic("Username cannot be empty.")
+	log.Info("Enter username:")
+	fmt.Print("Username: ")
+	username, _ := reader.ReadString('\n')
+	username = username[:len(username)-1]
+
+	log.Info("Enter password:")
+	fmt.Print("Password: ")
+	password, _ := reader.ReadString('\n')
+	password = password[:len(password)-1]
+
+	switch option[0] {
+	case '1':
+		log.Info("Registering...")
+		creds := models.UserRegisterInput{UserName: username, HashPassword: password}
+		url := serverURL + "/register"
+		response, err := sendRequest(url, creds)
+		if err != nil {
+			log.Error("Registration error: ", err)
+			return
+		}
+		log.Info("Registration response: ", response)
+	case '2':
+		log.Info("Logging in...")
+		creds := models.UserLoginInput{UserName: username, Password: password}
+		url := serverURL + "/login"
+		response, err := sendRequest(url, creds)
+		if err != nil {
+			log.Error("Login error: ", err)
+			return
+		}
+		log.Info("Login response: ", response)
+	default:
+		log.Info("Invalid option")
 	}
+}
 
-	url := "http://localhost:8080/login"
-	data := "username=" + username
-
-	const timeoutDuration = 10 * time.Second
-	ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
-
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBufferString(data))
+func sendRequest(url string, data interface{}) (string, error) {
+	jsonData, err := json.Marshal(data)
 	if err != nil {
-		log.WithError(err).Panic("Error creating request")
+		return "", err
 	}
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
-		log.WithError(err).Panic("Error sending request.")
+		return "", err
 	}
-
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.WithError(err).Panic("Error reading response.")
+		return "", err
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		log.Errorf("Failed to log in: %s\n", body)
-		log.WithError(errStatusNotOK).Panic("Failed to log in.")
+	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK {
+		var errResp models.ErrorResponse
+		if err := json.Unmarshal(body, &errResp); err != nil {
+			return "", err // Error unmarshalling the error response
+		}
+		return "", fmt.Errorf("server error: %s", errResp.Error)
 	}
 
-	log.Infof("Response from server: %s", string(body))
+	var response struct {
+		Data struct {
+			Token string               `json:"token"`
+			User  *models.UserResponse `json:"user,omitempty"`
+		} `json:"data"`
+		Token string `json:"token"` // For login response
+	}
+
+	if err := json.Unmarshal(body, &response); err != nil {
+		return "", err // Error unmarshalling the data response
+	}
+
+	// Displaying user data if available (for register response)
+	userDisplay := ""
+	if response.Data.User != nil {
+		userDisplay = fmt.Sprintf(", User: %s, CreatedAt: %s, UpdatedAt: %s",
+			response.Data.User.UserName, response.Data.User.CreatedAt, response.Data.User.UpdatedAt)
+	}
+
+	return fmt.Sprintf("Token: %s%s", response.Data.Token, userDisplay), nil
 }
