0eab8f0c12901d6fab844b06bc00b9dd369a8dfd
stsolovey <st.solovey@gmail.com>
2024-05-20 22:08:02 +0300

    NewTextFormat for client added

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -10,3 +10,12 @@ func New() *logrus.Logger {
 	// logger.SetLevel(logrus.InfoLevel)
 	return logger
 }
+
+func NewTextFormat() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	// logger.SetLevel(logrus.InfoLevel)
+	return logger
+}
