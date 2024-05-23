2cf9ca08e7b80ec51b3ce292fb90e9653e0cbb23
stsolovey <st.solovey@gmail.com>
2024-05-23 12:20:34 +0300

    Messase struct improved, Receiver-Sender fields added

--- internal/models/message.go
+++ internal/models/message.go
@@ -3,9 +3,10 @@ package models
 import "time"
 
 type Message struct {
-	ID        int       `json:"id"`
-	RoomID    int       `json:"roomId"`
-	Author    string    `json:"author"`
+	ID        int       `json:"id,omitempty"`
+	Receiver  string    `json:"receiver,omitempty"`
+	Sender    string    `json:"sender"`
 	Content   string    `json:"content"`
+	Error     string    `json:"error,omitempty"`
 	CreatedAt time.Time `json:"createdAt"`
 }
